8ccd66d8d17bf6b67e221e2ca289cad72da6f5ac
astridx <info@astrid-guenther.de>
2023-04-23 11:54:22 +0200

    wip script

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -9,14 +9,14 @@
 #
 
 # https://stackoverflow.com/posts/73100228/revisions
-# git config --global --add safe.directory /srv/tile/openstreetmap-carto-de
-# git config --global --add safe.directory /srv/tile/sources/osml10n
+git config --global --add safe.directory /srv/tile/openstreetmap-carto-de
+git config --global --add safe.directory /srv/tile/sources/osml10n
 
 CARTO_VERSION=$(git -C /srv/tile/openstreetmap-carto-de/ describe --tags)
 l10n_VERSION=$(git -C /srv/tile/sources/osml10n describe --tags)
 
 cd /srv/tile/site/assets
 
-sed -i 's/VITE_HOSTNAME/$HOSTNAME/g' *
-sed -i 's/VITE_OSML10N_VERSION/$l10n_VERSION/g' *
-sed -i 's/VITE_OPENSTREETMAP_CARTO_DE_VERSION/$CARTO_VERSION/g' *
+sudo sed -i "s/VITE_HOSTNAME/${HOSTNAME}/g" *
+sudo sed -i "s/VITE_OSML10N_VERSION/${l10n_VERSION}/g" *
+sudo sed -i "s/VITE_OPENSTREETMAP_CARTO_DE_VERSION/${CARTO_VERSION}/g" *
