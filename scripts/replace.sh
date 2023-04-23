4c608fe709f4a1cd8adee756142562f9a5ac2d32
astridx <info@astrid-guenther.de>
2023-04-23 16:15:48 +0200

    wip replace-script

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -14,6 +14,7 @@ git config --global --add safe.directory /srv/tile/sources/osml10n
 
 CARTO_VERSION=$(git -C /srv/tile/openstreetmap-carto-de/ describe --tags)
 l10n_VERSION=$(git -C /srv/tile/sources/osml10n describe --tags)
+HOSTNAME=$HOSTNAME
 
 cd /srv/tile/site/assets
 
